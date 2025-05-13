28b0b0e90fa34693351b0484da5f7ad2eae9d659
shoya6666 <shoya061666@icloud.com>
2025-05-13 12:38:06 +0900

    a

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <Novice.h>
 #include<Vector3.h>
-#include<math.h>
+#include<cmath>
 #include<Matrix4x4.h>
 #include <assert.h>
 
@@ -8,59 +8,16 @@ const char kWindowTitle[] = "GC1C_08_タナカ_ショウヤ_タイトル";
 const int kWindowWidth = 1280;
 const int kWindowHeight = 720;
 
-Matrix4x4 MakeTranslateMatrix(const Vector3& translate)
-{
-	Matrix4x4 result;
+Matrix4x4 MakeRotateXMatrix(float radian) {
 
-	result.m[0][0] = 1.0f;
-	result.m[0][1] = 0.0f;
-	result.m[0][2] = 0.0f;
-	result.m[0][3] = 0.0f;
+}
+Matrix4x4 MakeRotateYMatrix(float radian) {
 
-	result.m[1][0] = 0.0f;
-	result.m[1][1] = 1.0f;
-	result.m[1][2] = 0.0f;
-	result.m[1][3] = 0.0f;
+}
+Matrix4x4 MakeRotateZMatrix(float radian) {
 
-	result.m[2][0] = 0.0f;
-	result.m[2][1] = 0.0f;
-	result.m[2][2] = 1.0f;
-	result.m[2][3] = 0.0f;
-
-	result.m[3][0] = translate.x;
-	result.m[3][1] = translate.y;
-	result.m[3][2] = translate.z;
-	result.m[3][3] = 1.0f;
-
-	return result;
 }
 
-Matrix4x4 MakeScaleMatrix(const Vector3& scale)
-{
-	Matrix4x4 result;
-
-	result.m[0][0] = scale.x;
-	result.m[0][1] = 0.0f;
-	result.m[0][2] = 0.0f;
-	result.m[0][3] = 0.0f;
-
-	result.m[1][0] = 0.0f;
-	result.m[1][1] = scale.y;
-	result.m[1][2] = 0.0f;
-	result.m[1][3] = 0.0f;
-
-	result.m[2][0] = 0.0f;
-	result.m[2][1] = 0.0f;
-	result.m[2][2] = scale.z;
-	result.m[2][3] = 0.0f;
-
-	result.m[3][0] = 0.0f;
-	result.m[3][1] = 0.0f;
-	result.m[3][2] = 0.0f;
-	result.m[3][3] = 1.0f;
-
-	return result;
-}
 
 Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix) {
 	Vector3 result;
@@ -111,17 +68,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
 
-	Vector3 translate{ 4.1f,2.6f,0.8f };
-	Vector3 scale{ 1.5f,5.2f,7.3f };
-	Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
-	Matrix4x4 scaleMatrix = MakeScaleMatrix(scale);
-	Vector3 point{ 2.3f,3.8f,1.4f };
-	Matrix4x4 transformMatrix = {
-		1.0f,2.0f,3.0f,4.0f,
-		3.0f,1.0f,1.0f,2.0f,
-		1.0f,4.0f,2.0f,3.0f,
-		2.0f,2.0f,1.0f,3.0f };
-	Vector3 transformed = Transform(point, transformMatrix);
+	Vector3 rotate{ 4.1f,2.6f,0.8f };
+	Matrix4x4 rotateXMtrix = MakeRotateXMatrix(rotateXMtrix.x);
+	Matrix4x4 rotateYMtrix = MakeRotateYMatrix(rotateYMtrix.y);
+	Matrix4x4 rotateZMtrix = MakeRotateZMatrix(rotateZMtrix.z);
+	Matrix4x4 rotateXYZMtrix = Multiply(rotateXMtrix), Multiply(rotateYMtrix), Multiply(rotateZMtrix));
+	
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -144,9 +96,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
-		VectorScreenPrintf(0, 0, transformed, "transformed");
-		MatrixScreenPrintf(0, 40, translateMatrix, "translateMatrix");
-		MatrixScreenPrintf(0, kRowHeight * 7, scaleMatrix, "scaleMatrix");
+		MatrixScreenPrintf(0, 0, rotateXMtrix, "rotateXMtrix");
+		MatrixScreenPrintf(0, kRowHeight * 5, rotateYMtrix, "rotateYMtrix");
+		MatrixScreenPrintf(0, kRowHeight * 5 * 2, rotateZMtrix, "rotateZMtrix");
+		MatrixScreenPrintf(0, kRowHeight * 5 * 3, rotateXYZMtrix, "rotateXYZMtrix");
 
 		///
 		/// ↑描画処理ここまで
