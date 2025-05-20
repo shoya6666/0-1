b82effaddf82695586eacf0167128fdd46d86850
shoya6666 <shoya061666@icloud.com>
2025-05-20 12:16:50 +0900

    a

--- main.cpp
+++ main.cpp
@@ -162,7 +162,25 @@ Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip
 //ビューポート変換行列
 Matrix4x4 MakeViewportMatrix(float Left, float top, float width, float height, float minDepth,float maxDepth) {
 	Matrix4x4 result;
+	result.m[0][0] = width / 2;
+	result.m[0][1] = 0.0f;
+	result.m[0][2] = 0.0f;
+	result.m[0][3] = Left + width / 2;
 
+	result.m[1][0] = 0.0f;
+	result.m[1][1] = -height / 2;
+	result.m[1][2] = 0.0f;
+	result.m[1][3] = top + height / 2;
+
+	result.m[2][0] = 0.0f;
+	result.m[2][1] = 0.0f;
+	result.m[2][2] = maxDepth - minDepth;
+	result.m[2][3] = minDepth;
+
+	result.m[3][0] = 0.0f;
+	result.m[3][1] = 0.0f;
+	result.m[3][2] = 0.0f;
+	result.m[3][3] = 1.0f;
 	return result;
 }
 
