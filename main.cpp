2769f2fcb7bdf51c52aac4060bc18ae1283a8089
shoya6666 <shoya061666@icloud.com>
2025-05-20 12:11:47 +0900

    a

--- main.cpp
+++ main.cpp
@@ -138,7 +138,25 @@ Matrix4x4 MakeOrthographicMatrix(float Left, float top,float right, float bottom
 //透視投影行列
 Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip) {
 	Matrix4x4 result;
+	result.m[0][0] = aspectRatio / 1 * 1 / tan(fovY / 2);
+	result.m[0][1] = 0.0f;
+	result.m[0][2] = 0.0f;
+	result.m[0][3] = 0.0f;
 
+	result.m[1][0] = 0.0f;
+	result.m[1][1] = 1 / tan(fovY / 2);
+	result.m[1][2] = 0.0f;
+	result.m[1][3] = 0.0f;
+
+	result.m[2][0] = 0.0f;
+	result.m[2][1] = 0.0f;
+	result.m[2][2] = farClip / farClip - nearClip;
+	result.m[2][3] = -nearClip * farClip / farClip - nearClip;
+
+	result.m[3][0] = 0.0f;
+	result.m[3][1] = 0.0f;
+	result.m[3][2] = 1.0f;
+	result.m[3][3] = 0.0f;
 	return result;
 }
 //ビューポート変換行列
